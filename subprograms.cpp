dae0c58e59b9a2255189c98dbd1fca0e8cc3bebf
Melvin Cali <melgust@gmail.com>
2025-09-27 16:53:52 -0600

    Update

--- subprograms.cpp
+++ subprograms.cpp
@@ -3,10 +3,15 @@
 #include <fstream>
 using namespace std;
 
+string VOWELS = "aeiou";
+string CONSONATS = "bcdfghjklmnpqrstvwxyz";
+string SYMBOLS = "~!@#$%^&*()_+{}:?></?;'[]=-.";
+
 int factorial(int);
 void mostrarTexto(string);
 int addition(int, int);
 void procedureAddition(int, int);
+int countCharacters(string, string);
 
 int main() {
     int number, number2;
@@ -14,6 +19,16 @@ int main() {
     cin >> number;
     cout << "Ingrese segundo numero: ";
     cin >> number2;
+    cin.ignore();
+    string text;
+    cout << "Ingrese una frase: ";
+    getline(cin, text);
+    int total = countCharacters(text, VOWELS);
+    cout << "Tiene " << total << " vocales" << endl;
+    total = countCharacters(text, CONSONATS);
+    cout << "Tiene " << total << " consonantes" << endl;
+    total = countCharacters(text, SYMBOLS);
+    cout << "Tiene " << total << " caracteres especiales" << endl;
     int result = factorial(number);
     string message = to_string(number) + "! = " + to_string(result);
     mostrarTexto(message);
@@ -42,4 +57,19 @@ int addition(int x, int y) {
 void procedureAddition(int x, int y) {
     int result = x + y;
     cout << "\nSuma: " << result << endl;
+}
+
+int countCharacters(string input, string characters) {
+    int total = 0;
+    for (int i = 0; i < input.length(); i++)
+    {
+        for (int j = 0; j < characters.length(); j++)
+        {
+            if (input[i] == characters[j]) {
+                total++;
+                break;
+            }
+        }
+    }
+    return total;
 }
